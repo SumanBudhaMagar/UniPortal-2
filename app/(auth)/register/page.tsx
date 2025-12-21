150e34241cca0dd4dd3c9d40608aea1274ce93e4
Suman Budha Magar <budhasuman24@gmail.com>
2025-12-21 22:46:45 +0545

    remaining lefts folder strutures made

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import RegisterPageComponent from '@/components/forms/RegisterPageComponent'
 
 const page = () => {
   return (
     <div>
-      
+      <RegisterPageComponent/>
     </div>
   )
 }
